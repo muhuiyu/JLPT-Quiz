5a5880b9993c604f6a32f227d27496d472cf4efa
muhuiyu <muhuiyu.grace@gmail.com>
2024-02-05 11:32:16 +0800

    Test `LocalQuizMapper` delivers item with JSON items

--- JLPTQuizTests/LocalQuizMapperTests.swift
+++ JLPTQuizTests/LocalQuizMapperTests.swift
@@ -22,6 +22,38 @@ class LocalQuizMapperTests: XCTestCase {
         
         XCTAssertEqual(try LocalQuizMapper.map(emptyListJSON), [])
     }
+    
+    func test_map_deliversItemWithJSONItems() {
+        let item1 = makeItem(id: "id-1", 
+                             type: .grammar,
+                             level: .n1,
+                             question: "question-1",
+                             options: [
+                                OptionEntry(value: "question-1/option-1", linkedEntryID: "linkedEntryID-1", isAnswer: true)
+                             ])
+        
+        let item2 = makeItem(id: "id-2",
+                             type: .kanji,
+                             level: .n2,
+                             question: "question-2",
+                             options: [
+                                OptionEntry(value: "question-2/option-1", linkedEntryID: "linkedEntryID-2", isAnswer: true),
+                                OptionEntry(value: "question-2/option-3", linkedEntryID: "linkedEntryID-3", isAnswer: false)
+                             ])
+        
+        let item3 = makeItem(id: "id-3",
+                             type: .vocab,
+                             level: .n5,
+                             question: "question-4",
+                             options: [
+                                OptionEntry(value: "question-3/option-1", linkedEntryID: "linkedEntryID-4", isAnswer: true),
+                                OptionEntry(value: "question-3/option-2", linkedEntryID: "linkedEntryID-5", isAnswer: false),
+                                OptionEntry(value: "question-3/option-3", linkedEntryID: "linkedEntryID-6", isAnswer: true)
+                             ])
+        
+        let json1 = makeItemsJSON([item1.json, item2.json, item3.json])
+        XCTAssertEqual(try LocalQuizMapper.map(json1), [item1.model, item2.model, item3.model])
+    }
 }
 
 extension LocalQuizMapperTests {
