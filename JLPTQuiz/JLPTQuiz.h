39652a9875502c8b75a922ac593843b825c2a705
muhuiyu <muhuiyu.grace@gmail.com>
2024-02-05 08:20:42 +0800

    Initial Commit

---
+++ JLPTQuiz/JLPTQuiz.h
@@ -0,0 +1,18 @@
+//
+//  JLPTQuiz.h
+//  JLPTQuiz
+//
+//  Created by Mu Yu on 2/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JLPTQuiz.
+FOUNDATION_EXPORT double JLPTQuizVersionNumber;
+
+//! Project version string for JLPTQuiz.
+FOUNDATION_EXPORT const unsigned char JLPTQuizVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JLPTQuiz/PublicHeader.h>
+
+
