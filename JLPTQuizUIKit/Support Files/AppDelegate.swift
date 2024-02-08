f25000bfd86229183a0e05c174b7594d37d918d7
muhuiyu <muhuiyu.grace@gmail.com>
2024-02-08 10:22:06 +0700

    Add UIKit target for iOS

---
+++ JLPTQuizUIKit/Support Files/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  JLPTQuizUIKit
+//
+//  Created by Mu Yu on 2/8/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ JLPTQuizUIKit/Support Files/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  JLPTQuizUIKit
+//
+//  Created by Mu Yu on 2/8/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .orange
+        
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+    }
+}
+
